9aff197dd989d9bd9aaa57d29d12e09b528cdd47
ENDAVA\solariu <silviu.olariu@endava.com>
2020-05-05 19:28:38 +0300

    building good classes

---
+++ Acme.Biz/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+   public  class Product
+    {
+        public Product(int productId, string productName,string description)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Description = description;
+        }
+
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+
+        public string ShowProductDetails()
+        {
+            var details = "product: " + ProductName + " " + Description;
+            return details;
+        }
+
+    }
+}
---
+++ Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,29 @@
+﻿using Acme.Biz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.BizTests
+{
+    [TestClass()]
+  public class ProductTests
+    {
+        [TestMethod()]
+        public void ShowProductDetail()
+        {
+            //arrange
+            var product = new Product(1,"Paper","desc");
+         
+
+            //act
+            var result = product.ShowProductDetails();
+
+            //assert
+            Assert.AreEqual("product: Paper desc", result);
+            
+        }
+    }
+}
